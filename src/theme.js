f312e5f5832135faa952482359e2109cec15f64b
Pawel Owiesek <Owiesek.p@gmail.com>
2023-08-31 22:24:23 +0200

    add theme.js

---
+++ src/theme.js
@@ -0,0 +1,33 @@
+export const theme = {
+  fonts: {
+    familyBarlowCondensed: "Barlow Condensed",
+    familyBellefair: "Bellefair",
+  },
+
+  fontSize: {
+    small: "0.875rem",
+    normal: "1rem",
+    medium: "1.125rem",
+    large: "9.375rem",
+    heading1: "6.25rem",
+    heading2: "3.5rem",
+    heading3: "2rem",
+    heading4: "1.75rem",
+    heading5: "1.5rem",
+  },
+
+  colors: {
+    dark: "230 35% 7%",
+    light: "231 77% 90%",
+    white: "0 0% 100%",
+  },
+
+  gap: {
+    gapSmall: "0.875rem",
+    gapNormal: "1rem",
+    gapMedium: "1.5rem",
+    gapLarge: "2rem",
+    gapXL: "2.5rem",
+    gapXXL: "3rem",
+  },
+};
