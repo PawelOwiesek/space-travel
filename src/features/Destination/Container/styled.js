827d21c4c81181c266849a3295c977d298fca364
Pawel Owiesek <Owiesek.p@gmail.com>
2023-08-31 22:30:54 +0200

    component Destination with styles and Container

---
+++ src/features/Destination/Container/styled.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const StyledWrapper = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  background-image: url("https://i.postimg.cc/NFCxpRV9/background-destination-desktop.jpg");
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+  max-width: 100vw;
+  height: 100vh;
+
+  @media (max-width: 1024px) {
+    background-image: url("https://i.postimg.cc/wvLP4xYc/background-destination-tablet.jpg");
+  }
+
+  @media (max-width: 450px) {
+    background-image: url("https://i.postimg.cc/tJdKJqrC/background-destination-mobile.jpg");
+  }
+`;
---
+++ src/features/Destination/index.js
@@ -0,0 +1,12 @@
+import { Paragraph } from "./styled";
+import Container from "./Container";
+
+const Destination = () => {
+  return (
+    <Container>
+      <Paragraph>Hello</Paragraph>
+    </Container>
+  );
+};
+
+export default Destination;
