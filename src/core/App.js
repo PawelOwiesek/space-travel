f2c979ec7aca62c3cee1da728ee817617900a271
Pawel Owiesek <Owiesek.p@gmail.com>
2023-08-31 22:25:48 +0200

    add Router

--- src/core/App.js
+++ src/core/App.js
@@ -1,7 +1,30 @@
+import { HashRouter, Switch, Route, Redirect } from "react-router-dom";
+import Destination from "../features/Destination";
+import Navigation from "../common/Navigation";
+import Home from "../features/Home";
+import Crew from "../features/Crew";
+import Technology from "../features/Technology";
+
 function App() {
   return (
     <>
-      <div></div>
+      <HashRouter>
+        <Navigation
+          toHome={"/home"}
+          toDestination={"/destination"}
+          toCrew={"/crew"}
+          toTech={"/technology"}
+        />
+        <Switch>
+          <Route path="/home" component={Home} />
+          <Route path="/destination" component={Destination} />
+          <Route path="/crew" component={Crew} />
+          <Route path="/technology" component={Technology} />
+          <Route path="/">
+            <Redirect to="/home" />
+          </Route>
+        </Switch>
+      </HashRouter>
     </>
   );
 }
