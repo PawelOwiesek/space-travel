9f83bc41c7d3ffa6360391e2a25a0e175191d0db
Pawel Owiesek <Owiesek.p@gmail.com>
2023-09-03 21:26:34 +0200

    add buttons and styles

--- src/features/Crew/index.js
+++ src/features/Crew/index.js
@@ -1,23 +1,48 @@
 import Container from "./Container";
 import data from "../../data.json";
-import TilePersons from "../../common/tiles/TileDestination";
+import TilePersons from "../../common/tiles/TilePersons";
+import { Button, Buttons, Item, List } from "./styled";
+import { useState } from "react";
 
 const Crew = () => {
+  const [number, setNumber] = useState(0);
+  const handleTransformation = (number) => {
+    setNumber(number);
+  };
+
   return (
     <Container>
-      {data.crew.map((item) => (
-        <ul>
-          <li key={item.name}>
+      <List>
+        {data.crew.map((item) => (
+          <Item number={number} key={item.name}>
             <TilePersons
+              role={item.role}
               person={item.name}
               description={item.bio}
-              role={item.role}
               image={item.images.png}
               alt={item.name}
             />
-          </li>
-        </ul>
-      ))}
+            <Buttons>
+              <Button
+                active={number === 0}
+                onClick={() => handleTransformation(0)}
+              />
+              <Button
+                active={number === 1}
+                onClick={() => handleTransformation(1)}
+              />
+              <Button
+                active={number === 2}
+                onClick={() => handleTransformation(2)}
+              />
+              <Button
+                active={number === 3}
+                onClick={() => handleTransformation(3)}
+              />
+            </Buttons>
+          </Item>
+        ))}{" "}
+      </List>
     </Container>
   );
 };
---
+++ src/features/Crew/styled.js
@@ -0,0 +1,46 @@
+import { styled } from "styled-components";
+import { theme } from "../../theme";
+
+export const List = styled.ul`
+  list-style-type: none;
+  display: flex;
+  overflow: hidden;
+`;
+
+export const Item = styled.li`
+  display: flex;
+  width: 100vw !important;
+  position: relative;
+  left: ${({ number }) =>
+    number === 0
+      ? "0vw"
+      : number === 1
+      ? "-100vw"
+      : number === 2
+      ? "-200vw"
+      : "-300vw"};
+  transition: 2s linear;
+`;
+export const Buttons = styled.div`
+  display: flex;
+  margin-top: 7.5rem;
+  gap: ${theme.gap.gapMedium};
+  position: absolute;
+  left: 13rem;
+  top: 43rem;
+  z-index: 4;
+`;
+
+export const Button = styled.button`
+  color: hsl(${theme.colors.white});
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  opacity: ${({ active }) => (active ? 1 : 0.174363)};
+  transition: 0.4s;
+
+  &:hover {
+    opacity: 1;
+    transform: scale(1);
+  }
+`;
