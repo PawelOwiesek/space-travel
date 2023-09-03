afa9471686ed57b4a4f71267d172cd9b52f0045f
Pawel Owiesek <Owiesek.p@gmail.com>
2023-09-03 15:06:07 +0200

    add Crew component

--- src/features/Crew/index.js
+++ src/features/Crew/index.js
@@ -1,9 +1,23 @@
 import Container from "./Container";
+import data from "../../data.json";
+import TilePersons from "../../common/tiles/TileDestination";
 
 const Crew = () => {
   return (
     <Container>
-      <p>Hello</p>
+      {data.crew.map((item) => (
+        <ul>
+          <li key={item.name}>
+            <TilePersons
+              person={item.name}
+              description={item.bio}
+              role={item.role}
+              image={item.images.png}
+              alt={item.name}
+            />
+          </li>
+        </ul>
+      ))}
     </Container>
   );
 };
