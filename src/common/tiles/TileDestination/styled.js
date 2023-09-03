47455418975b5c8dd016f884accae91b3e57366f
Pawel Owiesek <Owiesek.p@gmail.com>
2023-09-03 15:09:55 +0200

    add styles for images

---
+++ src/common/tiles/TileDestination/styled.js
@@ -0,0 +1,7 @@
+import { styled } from "styled-components";
+
+export const Image = styled.img`
+  width: 300px;
+  height: 300px;
+  position: relative;
+`;
---
+++ src/common/tiles/TilePersons/styled.js
@@ -0,0 +1,7 @@
+import { styled } from "styled-components";
+
+export const ImagePerson = styled.img`
+  width: 300px;
+  height: 300px;
+  padding-top: 30px;
+`;
