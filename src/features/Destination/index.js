22b0c35bf3b35b02fa8fc1536741be04e98f2116
Pawel Owiesek <Owiesek.p@gmail.com>
2023-09-03 15:09:00 +0200

    add data from data.json

--- src/features/Destination/index.js
+++ src/features/Destination/index.js
@@ -1,11 +1,27 @@
-import { Image } from "./styled";
 import Container from "./Container";
+import Tile from "../../common/tiles/TileDestination";
+import data from "../../data.json";
 
 const Destination = () => {
   return (
-    <Container>
-      <Image src="https://i.postimg.cc/GtkcCtXh/image-mars.png" alt="mars" />
-    </Container>
+    <>
+      <Container>
+        {data.destinations.map((item) => (
+          <ul>
+            <li key={item.name}>
+              <Tile
+                planet={item.name}
+                description={item.description}
+                distance={item.distance}
+                travel={item.travel}
+                image={item.images.png}
+                alt={item.name}
+              />
+            </li>
+          </ul>
+        ))}
+      </Container>
+    </>
   );
 };
 
