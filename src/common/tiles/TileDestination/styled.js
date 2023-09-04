2bd3f850f04d1af5909eb98b316006c906b519d9
Pawel Owiesek <Owiesek.p@gmail.com>
2023-09-04 11:42:55 +0200

    rotaet animation

--- src/common/tiles/TileDestination/styled.js
+++ src/common/tiles/TileDestination/styled.js
@@ -3,5 +3,13 @@ import { styled } from "styled-components";
 export const Image = styled.img`
   width: 300px;
   height: 300px;
-  position: relative;
+  animation: spin 200s linear infinite;
+  @keyframes spin {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
 `;
