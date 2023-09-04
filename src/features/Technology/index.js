06b374809da7d56bfbded6abc82edd7c57c72a7b
Pawel Owiesek <Owiesek.p@gmail.com>
2023-09-04 11:42:30 +0200

    add technology content

--- src/features/Technology/index.js
+++ src/features/Technology/index.js
@@ -1,9 +1,22 @@
 import Container from "./Container";
+import data from "../../data.json";
+import { Tile } from "../../common/tiles/TileTechnologies";
 
 const Technology = () => {
   return (
     <Container>
-      <p>Hello</p>
+      {data.technology.map((item) => (
+        <ul>
+          <li key={item.name}>
+            <Tile
+              name={item.name}
+              description={item.description}
+              image={item.images.portrait}
+              alt={item.name}
+            />
+          </li>
+        </ul>
+      ))}
     </Container>
   );
 };
