0bd6ce5e168480b0da53811d9f2276c8bca56d4b
Pawel Owiesek <Owiesek.p@gmail.com>
2023-09-04 11:41:51 +0200

    styles for mobile

--- src/features/Home/styled.js
+++ src/features/Home/styled.js
@@ -2,17 +2,25 @@ import { styled } from "styled-components";
 import { theme } from "../../theme";
 import { Link } from "react-router-dom";
 
-// export const Wrapper = styled.div`
-//   display: flex;
-//   justify-content: space-around;
-//   align-items: center;
-//   margin-top: 300px;
-// `;
 export const Content = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: space-between;
   gap: ${theme.gap.gapMedium};
+
+  @media (max-width: ${theme.breakpoints.large}) {
+    justify-content: center;
+    align-items: center;
+    gap: ${theme.gap.gapSmall};
+    margin: 70px 0 0 50px;
+  }
+
+  @media (max-width: ${theme.breakpoints.medium}) {
+    justify-content: center;
+    align-items: center;
+    gap: ${theme.gap.gapMedium};
+    margin: 0;
+  }
 `;
 export const Header = styled.h2`
   font-size: ${theme.fontSize.heading4};
@@ -21,17 +29,46 @@ export const Header = styled.h2`
   letter-spacing: 0.29531rem;
   line-height: normal;
   font-family: ${theme.fonts.familyBarlowCondensed};
+
+  @media (max-width: ${theme.breakpoints.large}) {
+    font-size: ${theme.fontSize.normal};
+    letter-spacing: 0.16875rem;
+  }
 `;
 
 export const LargeHeader = styled(Header)`
   font-size: ${theme.fontSize.large};
   font-family: ${theme.fonts.familyBellefair};
   color: hsl(${theme.colors.white});
+
+  @media (max-width: ${theme.breakpoints.ipadSmall}) {
+    font-size: ${theme.fontSize.heading2};
+    line-height: 4.25rem;
+  }
+
+  @media (max-width: ${theme.breakpoints.medium}) {
+    font-size: ${theme.fontSize.heading2};
+    line-height: 3.5rem;
+  }
+
+  @media (max-width: ${theme.breakpoints.small}) {
+    font-size: ${theme.fontSize.heading3};
+    line-height: 2rem;
+  }
 `;
 
 export const Description = styled.p`
   color: hsl(${theme.colors.light});
   width: 27.75rem;
+
+  @media (max-width: ${theme.breakpoints.large}) {
+    width: 20rem;
+    text-align: center;
+  }
+
+  @media (max-width: ${theme.breakpoints.medium}) {
+    width: 18rem;
+  }
 `;
 
 export const StyledLink = styled(Link)`
@@ -39,7 +76,6 @@ export const StyledLink = styled(Link)`
 `;
 
 export const Text = styled.div`
-  display: none;
   position: relative;
   z-index: 4;
   display: grid;
@@ -60,6 +96,20 @@ export const Text = styled.div`
   &:hover {
     background-color: hsl(${theme.colors.dark} / 56%);
   }
+
+  @media (max-width: ${theme.breakpoints.ipadSmall}) {
+  }
+
+  @media (max-width: ${theme.breakpoints.large}) {
+    font-size: ${theme.fontSize.medium};
+    height: 12px;
+    margin-top: 50px;
+    max-width: 150px;
+  }
+
+  @media (max-width: ${theme.breakpoints.medium}) {
+    margin-top: 100px;
+  }
 `;
 
 export const LinkBackground = styled.div`
@@ -69,4 +119,14 @@ export const LinkBackground = styled.div`
   height: 17.125rem;
   border-radius: 50%;
   background-color: hsl(${theme.colors.white});
+
+  @media (max-width: ${theme.breakpoints.ipadSmall}) {
+    width: 11.375rem;
+    height: 11.375rem;
+  }
+
+  @media (max-width: ${theme.breakpoints.large}) {
+    width: 9.375rem;
+    height: 9.375rem;
+  }
 `;
