a507297fdd65c14232b343d938bed34171362757
Pawel Owiesek <Owiesek.p@gmail.com>
2023-09-06 08:34:18 +0200

    add styles to list

--- src/features/Destination/styled.js
+++ src/features/Destination/styled.js
@@ -2,4 +2,15 @@ import { styled } from "styled-components";
 
 export const List = styled.ul`
   list-style-type: none;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+`;
+
+export const Item = styled.div`
+  display: grid;
+  width: 100vw;
+  align-items: center;
+  justify-items: center;
+  grid-template-columns: repeat(2, 1fr);
+  color: white;
 `;
