2c442a616f3295913cf96ddb43ac6b03ecd46cfc
Pawel Owiesek <Owiesek.p@gmail.com>
2023-09-06 11:48:25 +0200

    add buttons and interactions

--- src/features/Destination/index.js
+++ src/features/Destination/index.js
@@ -1,15 +1,49 @@
 import Container from "./Container";
 import Tile from "../../common/tiles/TileDestination";
 import data from "../../data.json";
-import { Item, List } from "./styled";
+import { Button, ButtonsWrapper, Item, List } from "./styled";
+import { useState } from "react";
 
 const Destination = () => {
+  const [number, setNumber] = useState(0);
+
+  const handleTransformation = (number) => {
+    setNumber(number);
+  };
+
   return (
     <>
       <Container>
-        {data.destinations.map((item) => (
-          <List>
-            <Item key={item.name}>
+        {" "}
+        <List>
+          {data.destinations.map((item) => (
+            <Item number={number} key={item.name}>
+              <ButtonsWrapper>
+                <Button
+                  active={number === 0}
+                  onClick={() => handleTransformation(0)}
+                >
+                  moon
+                </Button>
+                <Button
+                  active={number === 1}
+                  onClick={() => handleTransformation(1)}
+                >
+                  mars
+                </Button>
+                <Button
+                  active={number === 2}
+                  onClick={() => handleTransformation(2)}
+                >
+                  europa
+                </Button>
+                <Button
+                  active={number === 3}
+                  onClick={() => handleTransformation(3)}
+                >
+                  titan
+                </Button>
+              </ButtonsWrapper>{" "}
               <Tile
                 planet={item.name}
                 description={item.description}
@@ -19,8 +53,8 @@ const Destination = () => {
                 alt={item.name}
               />
             </Item>
-          </List>
-        ))}
+          ))}
+        </List>
       </Container>
     </>
   );
