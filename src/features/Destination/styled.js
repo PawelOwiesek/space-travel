46299e4825745e0ab22d15ca8b0337811f0f3404
Pawel Owiesek <Owiesek.p@gmail.com>
2023-09-06 11:49:08 +0200

    buttons and li styles for small devices

--- src/features/Destination/styled.js
+++ src/features/Destination/styled.js
@@ -1,16 +1,69 @@
 import { styled } from "styled-components";
+import { theme } from "../../theme";
 
 export const List = styled.ul`
   list-style-type: none;
   display: grid;
   grid-template-columns: repeat(4, 1fr);
+  padding: 0;
+  overflow: hidden;
+  margin: 0;
 `;
 
-export const Item = styled.div`
+export const Item = styled.li`
   display: grid;
   width: 100vw;
   align-items: center;
   justify-items: center;
   grid-template-columns: repeat(2, 1fr);
   color: white;
+
+  @media (max-width: ${theme.breakpoints.small}) {
+    display: flex;
+    flex-direction: column;
+    position: relative;
+    left: ${({ number }) =>
+      number === 0
+        ? "0vw"
+        : number === 1
+        ? "-100vw"
+        : number === 2
+        ? "-200vw"
+        : "-300vw"};
+    transition: 2s linear;
+  }
+`;
+export const ButtonsWrapper = styled.div`
+  @media (max-width: ${theme.breakpoints.small}) {
+    display: flex;
+    justify-content: center;
+    gap: ${theme.gap.gapSmall};
+    position: fixed;
+    top: 230px;
+  }
+`;
+
+export const Button = styled.button`
+  background-color: transparent;
+  color: hsl(${theme.colors.light} / 88%);
+  text-transform: uppercase;
+  font-weight: 400;
+  letter-spacing: 0.14rem;
+  font-size: ${theme.fontSize.small};
+  font-family: ${theme.fonts.familyBarlowCondensed};
+  border: none;
+  border-bottom: 0.25rem solid transparent;
+
+  &:hover {
+    border-bottom: 0.25rem solid hsl(${theme.colors.light} / 20%);
+    padding: 0.29rem 0;
+
+    @media (max-width: ${theme.breakpoints.ipadSmall}) {
+      padding: 0.29rem 0;
+      border-bottom: 0.25rem solid hsl(${theme.colors.white});
+    }
+  }
+  border-bottom: ${({ active }) =>
+    active ? `0.25rem solid hsl(${theme.colors.white})` : null};
+  padding: 0.29rem 0;
 `;
