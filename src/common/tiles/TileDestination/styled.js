c2457a4d15e5642d34403df0b123501d14d6d6d0
Pawel Owiesek <Owiesek.p@gmail.com>
2023-09-06 08:31:02 +0200

    add sytled Wrapper

--- src/common/tiles/TileDestination/styled.js
+++ src/common/tiles/TileDestination/styled.js
@@ -1,4 +1,5 @@
 import { styled } from "styled-components";
+import { theme } from "../../../theme";
 
 export const Image = styled.img`
   width: 300px;
@@ -11,5 +12,39 @@ export const Image = styled.img`
     100% {
       transform: rotate(360deg);
     }
+  }
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
+  gap: ${theme.gap.gapSmall};
+`;
+
+export const Button = styled.button`
+  background-color: transparent;
+  color: hsl(${theme.colors.light} / 88%);
+  text-transform: uppercase;
+  font-weight: 400;
+  letter-spacing: 0.14rem;
+  font-size: ${theme.fontSize.small};
+  font-family: ${theme.fonts.familyBarlowCondensed};
+  border: none;
+  border-bottom: 4px solid transparent;
+
+  &:hover {
+    border-bottom: 4px solid hsl(${theme.colors.light} / 20%);
+    padding: 5px 0;
+    @media (max-width: ${theme.breakpoints.ipadSmall}) {
+      border: none;
+      padding: 0;
+    }
+  }
+  border-bottom: ${({ index }) =>
+    index ? `4px solid hsl(${theme.colors.white})` : null};
+  padding: 5px 0;
+
+  @media (max-width: ${theme.breakpoints.ipadSmall}) {
+    border: none;
+    padding: 0;
   }
 `;
